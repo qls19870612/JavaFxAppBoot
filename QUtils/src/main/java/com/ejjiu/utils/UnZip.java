6930c5ca6fdae8dd695ebef002e4b2b2d6d34330
qiuliangsong <qls19870612@163.com>
2023-11-08 16:07:33 +0800

    lombok升级  zip解压BUG修复

--- QUtils/src/main/java/com/ejjiu/utils/UnZip.java
+++ QUtils/src/main/java/com/ejjiu/utils/UnZip.java
@@ -5,6 +5,10 @@ package com.ejjiu.utils;
  * 创建人  liangsong
  * 创建时间 2021/03/05 10:34
  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,14 +20,12 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 
-public class UnZip
-{
+public class UnZip {
     public static byte[] decompress(byte[] data) throws IOException, DataFormatException {
         Inflater inflater = new Inflater();
         inflater.setInput(data);
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(
-                data.length);
+        
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
         byte[] buffer = new byte[4096];
         while (!inflater.finished()) {
             int count = inflater.inflate(buffer);
@@ -33,58 +35,58 @@ public class UnZip
         outputStream.close();
         return output;
     }
-
-
-
-        public static void unzip(String zipFilePath, String destDir)
-        {
-            System.setProperty("sun.zip.encoding", System.getProperty("sun.jnu.encoding")); //Error preventing Chinese from being in the file name
-            //System.out.println(System.getProperty("sun.zip.encoding")); //ZIP encoding
-            // System.out.println (System.getProperty ("sun.jnu.encoding")); // current file encoding
-            //System.out.println(System.getProperty("file.encoding")); //This is the encoding method of the current file content.
-
-            File dir = new File(destDir);
-            // create output directory if it doesn't exist
-            if (!dir.exists()) dir.mkdirs();
-            FileInputStream fis;
-            // buffer for read and write data to file
-            byte[] buffer = new byte[1024];
-            try
-            {
-                fis = new FileInputStream(zipFilePath);
-                ZipInputStream zis = new ZipInputStream(fis);
-                ZipEntry ze = zis.getNextEntry();
-                while (ze != null)
-                {
-                    String fileName = ze.getName();
-                    File newFile = new File(destDir + File.separator + fileName);
-                    //System.out.println("Unzipping to " + newFile.getAbsolutePath());
-                    // create directories for sub directories in zip
-                    new File(newFile.getParent()).mkdirs();
+    
+    
+    private static final Logger logger = LoggerFactory.getLogger(UnZip.class);
+    
+    public static void unzip(String zipFilePath, String destDir) {
+        System.setProperty("sun.zip.encoding", System.getProperty("sun.jnu.encoding")); //Error preventing Chinese from being in the file name
+        //System.out.println(System.getProperty("sun.zip.encoding")); //ZIP encoding
+        // System.out.println (System.getProperty ("sun.jnu.encoding")); // current file encoding
+        //System.out.println(System.getProperty("file.encoding")); //This is the encoding method of the current file content.
+        
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        FileInputStream fis;
+        // buffer for read and write data to file
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                //System.out.println("Unzipping to " + newFile.getAbsolutePath());
+                // create directories for sub directories in zip
+                
+                if (!ze.isDirectory()) {
                     FileOutputStream fos = new FileOutputStream(newFile);
                     int len;
-                    while ((len = zis.read(buffer)) > 0)
-                    {
+                    while ((len = zis.read(buffer)) > 0) {
                         fos.write(buffer, 0, len);
                     }
                     fos.close();
-                    // close this ZipEntry
-                    zis.closeEntry();
-                    ze = zis.getNextEntry();
+                } else {
+                    newFile.mkdirs();
                 }
-                // close last ZipEntry
+                
+                // close this ZipEntry
                 zis.closeEntry();
-                zis.close();
-                fis.close();
+                ze = zis.getNextEntry();
             }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-
+            // close last ZipEntry
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
-
+        
+    }
+    
+    
 }
